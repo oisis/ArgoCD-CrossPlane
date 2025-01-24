84e1a9d2b111b6051e19cdb3deb41e3a84e9553b
OiSiS <oisisk@gmail.com>
2025-01-24 22:58:01 +0100

    [argocd] Translate outputs to eng

--- run.sh
+++ run.sh
@@ -28,16 +28,16 @@ printf "${GREEN}Open http://localhost:8080 in webbrowser${NC}\n"
 
 printf "${GREEN}Create AWS credentials for CrossPlane:${NC}\n"
 if [ -z "${AWS_ACCESS_KEY_ID}" ]; then
-  read -p "Podaj wartość dla zmiennej VAR_A: " AWS_ACCESS_KEY_ID
+  read -p "Provide a value for the variable AWS_ACCESS_KEY_ID: " AWS_ACCESS_KEY_ID
 fi
 
 if [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then
-  read -p "Podaj wartość dla zmiennej VAR_A: " AWS_SECRET_ACCESS_KEY
+  read -p "Provide a value for the variable AWS_SECRET_ACCESS_KEY: " AWS_SECRET_ACCESS_KEY
 fi
 
 if ! kubectl get namespace crossplane-system &> /dev/null; then
   kubectl create namespace crossplane-system
-  echo "Namespace crossplane-system został utworzony."
+  echo "The namespace crossplane-system has been created."
 fi
 
 kubectl create secret generic aws-secret \
